0f2004983cc24951832d0cc3a57dd2e7e16d42d8
Oliver Sherouse <oliver@oliversherouse.com>
2019-04-10 22:25:45 -0400

    inaugurated dev branch one commit late

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Oliver Sherouse"
 # The short X.Y version
 version = "0.1"
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.1.2.dev"
 
 
 # -- General configuration ---------------------------------------------------
